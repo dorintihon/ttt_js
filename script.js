29113fb0581347060af5bb809079869e310bb005
Dorin <gimiri99@gmail.com>
2025-04-02 15:16:44 -0400

    Added gamelogic object

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-const Gameboard = {
-    board: new Array(9).fill(null),
+const Gameboard = (() => {
+    const board = new Array(9).fill(null);
 
-    printBoard: function() {
-        console.log(this.board);
-    },
+    const printBoard = () => {
+        console.log(board);
+    };
 
-    resetBoard: function() {
-        this.board = new Array(9).fill(null);
-    },
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = null;
+        }
+    };
 
-    return{
+    const setMark = (index, mark) => {
+        if (board[index] === null) {
+            board[index] = mark;
+        }
+    };
+
+    return {
         printBoard,
-        resetBoard
-    }
-}
+        resetBoard,
+        setMark
+    };
+})();
+
 
 const Player = (name, mark) => {
     return {name, mark}
 }
+
+const GameLogic = (() => {
+    const players = [Player('Player 1', 'X'), Player('Player 2', 'O')];
+    let currentPlayer = players[0];
+
+    const changePlayer = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    };
+
+    return {
+        changePlayer,
+        getCurrentPlayer
+    };
+})();
 
 
 
