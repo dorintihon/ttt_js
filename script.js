1982e2a9ae18c48761e10c6e03dfd87397924d02
Dorin <gimiri99@gmail.com>
2025-04-02 15:13:42 -0400

    Created Gameboard and Player object

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-var Gameboard = (function() {
-    
-})();
+const Gameboard = {
+    board: new Array(9).fill(null),
+
+    printBoard: function() {
+        console.log(this.board);
+    },
+
+    resetBoard: function() {
+        this.board = new Array(9).fill(null);
+    },
+
+    return{
+        printBoard,
+        resetBoard
+    }
+}
+
+const Player = (name, mark) => {
+    return {name, mark}
+}
+
+
+
